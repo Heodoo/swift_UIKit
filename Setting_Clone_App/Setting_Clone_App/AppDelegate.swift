1a5df4983a1459335eec07ff144441e311fb1b6b
Heodoo <39407635+Heodoo@users.noreply.github.com>
2022-01-13 15:42:34 +0900

    setting clone app prac
    iphone setting app clone pratice 22/01/12
    
    나중에 더 추가하기

---
+++ Setting_Clone_App/Setting_Clone_App/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  Setting_Clone_App
+//
+//  Created by 허두영 on 2022/01/12.
+//
+//AppDelegate는 앱의 라이프사이클을 관리, 어떤 notification이 와서 앱이 실행되거나 닫히는 그 순간
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Setting_Clone_App/Setting_Clone_App/SettingModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingModel.swift
+//  Setting_Clone_App
+//
+//  Created by 허두영 on 2022/01/12.
+//
+
+import Foundation
+
+
+struct SettingModel {
+    //있을수도 없을수도 있는 정보는 옵셔널로 표현
+    var leftImageName : String = ""
+    var menuTitle: String = ""
+    var subTitle : String?
+    var rightImageName : String?
+
+}
